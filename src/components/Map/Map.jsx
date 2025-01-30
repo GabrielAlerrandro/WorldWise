a5ce21da6cf47a774bb2004539bba7245b251b60
Gabriel Alerrandro <gabrielalerrandro081@gmail.com>
2025-01-30 16:12:56 -0300

    Geolocation

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useSearchParams } from "react-router-dom"
-
+import Button from "../../components/Button/Button"
 import styles from "./Map.module.css"
 import {
   MapContainer,
@@ -11,9 +11,15 @@ import {
 } from "react-leaflet"
 import { useEffect, useState } from "react"
 import { useCities } from "../../context/CitiesContext"
+import { useGeolocation } from "../../hooks/useGeolocation"
 
 export default function Map() {
   const { cities } = useCities()
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation()
 
   const [mapPosition, setMapPosition] = useState([40, 0])
   const [searchParams] = useSearchParams()
@@ -25,8 +31,18 @@ export default function Map() {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
   }, [mapLat, mapLng])
 
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+  }, [geolocationPosition])
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={() => getPosition()}>
+          {isLoadingPosition ? "Loading.." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={13}
